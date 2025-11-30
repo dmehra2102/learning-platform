ddc59448be3a0eea46a031a72b12436a7c06d797
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2025-11-30 20:08:56 +0530

    feat: added lesson repository logic

---
+++ course-service/internal/repository/lesson_repository.go
@@ -0,0 +1,112 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/dmehra2102/learning-platform/course-service/internal/domain"
+	"github.com/dmehra2102/learning-platform/shared/pkg/database"
+)
+
+type LessonRepository interface {
+	Create(ctx context.Context, lesson *domain.Lesson) error
+	GetByID(ctx context.Context, id string) (*domain.Lesson, error)
+	GetByModuleID(ctx context.Context, moduleID string) ([]*domain.Lesson, error)
+	Update(ctx context.Context, lesson *domain.Lesson) error
+	Delete(ctx context.Context, id string) error
+}
+
+type lessonRepository struct {
+	db *database.DB
+}
+
+func NewLessonRepository(db *database.DB) LessonRepository {
+	return &lessonRepository{db: db}
+}
+
+func (r *lessonRepository) Create(ctx context.Context, lesson *domain.Lesson) error {
+	query := `
+		INSERT INTO lessons (id, module_id, title,description, video_id, duration_seconds, order_index, is_preview, created_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+	`
+
+	_, err := r.db.ExecContext(ctx, query,
+		lesson.ID, lesson.ModuleID, lesson.Title, lesson.Description,
+		lesson.VideoID, lesson.DurationSeconds, lesson.OrderIndex, lesson.IsPreview, lesson.CreatedAt,
+	)
+
+	return err
+}
+
+func (r *lessonRepository) GetByID(ctx context.Context, id string) (*domain.Lesson, error) {
+	query := `SELECT id, module_id, title, description, video_id, duration_seconds, 	 order_index, is_preview, created_at FROM lessons WHERE id = $1`
+
+	var lesson domain.Lesson
+	err := r.db.QueryRowContext(ctx, query, id).Scan(
+		&lesson.ID, &lesson.ModuleID, &lesson.Title, &lesson.Description,
+		&lesson.VideoID, &lesson.DurationSeconds, &lesson.OrderIndex, &lesson.IsPreview, &lesson.CreatedAt,
+	)
+
+	if err == sql.ErrNoRows {
+		return nil, domain.ErrCourseNotFound
+	}
+
+	return &lesson, err
+}
+
+func (r *lessonRepository) GetByModuleID(ctx context.Context, moduleID string) ([]*domain.Lesson, error) {
+	query := `
+		SELECT id, module_id, title, description, video_id, duration_seconds, order_index, is_preview, created_at FROM lessons WHERE module_id = $1 ORDER BY order_index
+	`
+
+	rows, err := r.db.QueryContext(ctx, query, moduleID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var lessons []*domain.Lesson
+	for rows.Next() {
+		var lesson domain.Lesson
+		if err := rows.Scan(
+			&lesson.ID, &lesson.ModuleID, &lesson.Title, &lesson.Description,
+			&lesson.VideoID, &lesson.DurationSeconds, &lesson.OrderIndex, &lesson.IsPreview, &lesson.CreatedAt,
+		); err != nil {
+			return nil, err
+		}
+
+		lessons = append(lessons, &lesson)
+	}
+
+	return lessons, nil
+}
+
+func (r *lessonRepository) Update(ctx context.Context, lesson *domain.Lesson) error {
+	query := `UPDATE lessons SET title = $1, description = $2, order_index = $3, is_preview = $4 WHERE id = $5`
+
+	result, err := r.db.ExecContext(ctx, query, lesson.Title, lesson.Description, lesson.OrderIndex, lesson.IsPreview, lesson.ID)
+	if err != nil {
+		return err
+	}
+
+	rows, _ := result.RowsAffected()
+	if rows == 0 {
+		return domain.ErrCourseNotFound
+	}
+
+	return nil
+}
+
+func (r *lessonRepository) Delete(ctx context.Context, id string) error {
+	query := `DELETE FROM lessons WHERE id = $1`
+	result, err := r.db.ExecContext(ctx, query, id)
+	if err != nil {
+		return err
+	}
+
+	rows, _ := result.RowsAffected()
+	if rows == 0 {
+		return domain.ErrCourseNotFound
+	}
+
+	return nil
+}
